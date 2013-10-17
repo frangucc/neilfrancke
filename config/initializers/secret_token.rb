782398be6ed0e9a8a986c3b5f6b599a2040b4769
banafederico <federico@neo.com>
2013-10-17 16:47:07 -0200

    Adds rails and moves only necessary files into /public.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Neilfrancke::Application.config.secret_key_base = 'd3dd0ae6be54d474d0ae4e541e2bdc06325f0e8e24d1302d6344b9574c1e40a5a1791c3ddb863046792bd2b82607c21180b9ceb64ae68640e5b704e7350f3362'
